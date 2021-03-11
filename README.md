bf344abb198f0b9b3d697ede4bfd89b051ea8096
Aleksandr Serdyuk <a.galko83@gmail.com>
2021-03-11 21:12:19 +0300

    add: added the ability to use relative and absolute paths to files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/AlexSerdyuk83/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/AlexSerdyuk83/frontend-project-lvl2/actions)
+
+https://asciinema.org/a/WiodppC1bX4QSqO5iUoH1uTlE
--- src/convert.js
+++ src/convert.js
@@ -1,9 +1,11 @@
 import { readFileSync } from 'fs';
-import { resolve } from 'path';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const convert = (fileName) => {
-  const currDirect = process.cwd();
-  const path = resolve(currDirect, 'src', fileName);
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const path = resolve(__dirname, fileName);
   const content = readFileSync(path, 'utf8');
   return JSON.parse(content);
 };
