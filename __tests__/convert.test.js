9f6e76c8ac6bcce7587abe2d69dd67126f57b3e7
Aleksandr Serdyuk <a.galko83@gmail.com>
2021-03-13 14:09:06 +0300

    fix: fixed bags for linting

--- __tests__/convert.test.js
+++ __tests__/convert.test.js
@@ -1,22 +1,21 @@
 import convert from '../src/convert.js';
-import { it, expect } from '@jest/globals';
+/* import { it, expect } from 'jest'; */
 
 it('convert_json', () => {
   const expected1 = {
-    host: "hexlet.io",
+    host: 'hexlet.io',
     timeout: 50,
-    proxy: "123.234.53.22",
-    follow: false
+    proxy: '123.234.53.22',
+    follow: false,
   };
   const expected2 = {
     timeout: 20,
     verbose: true,
-    host: "hexlet.io"
+    host: 'hexlet.io',
   };
   const actual1 = convert('file1.json');
   expect(actual1).toEqual(expected1);
 
   const actual2 = convert('file2.json');
-  expect(actual2).toEqual(expected2);;
-
+  expect(actual2).toEqual(expected2);
 });
--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -1,18 +1,18 @@
-import genDiff from '../src/diff.js';
 import { readFileSync } from 'fs';
 import { resolve, dirname } from 'path';
 import { fileURLToPath } from 'url';
-import { it, expect } from '@jest/globals';
+import genDiff from '../src/diff.js';
+/* import { it, expect } from 'jest'; */
 
 const readTxt = (fileName) => {
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = dirname(__filename);
-    const path = resolve(__dirname, fileName);
-    return readFileSync(path, 'utf8');
-  };
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const path = resolve(__dirname, fileName);
+  return readFileSync(path, 'utf8');
+};
 
 it('finding the difference between files', () => {
-    const expected = genDiff('file1.json', 'file2.json');
-    const actual = readTxt('__fixtures__/result_diff.txt');
-    expect(actual).toEqual(expected);
-  });
+  const expected = genDiff('file1.json', 'file2.json');
+  const actual = readTxt('__fixtures__/result_diff.txt');
+  expect(actual).toEqual(expected);
+});
